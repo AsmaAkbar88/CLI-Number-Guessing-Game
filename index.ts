167c525f56591cffd92934248aebd628162c357d
AsmaAkbar88 <164855203+AsmaAkbar88@users.noreply.github.com>
2024-04-09 12:07:49 +0500

    Created number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+
+
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 9 + 1)
+const answer = await inquirer.prompt([
+{name: "userGuessingNumber",
+    type: "number",
+    message: "Please guess a number between 1-9: ",
+},
+
+])
+if(answer.userGuessingNumber === randomNumber)
+    {console.log("Congratulation! You guess the righ number")
+console.log("you won the game")}
+
+else {console.log("you guess the wrong number")
+ console.log("Try again")}
