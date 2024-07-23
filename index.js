b1e4eeaa8a101444d1948ca83df9f87f3295c734
Asma Akbar <masoomlarki297@gmail.com>
2024-07-23 18:36:41 +0500

    created new style number guessing game

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const randomNumber = Math.floor(Math.random() * 9 + 1);
 const answer = await inquirer.prompt([
     { name: "userGuessingNumber",
         type: "number",
-        message: "Please guess a number between 1-9: ",
+        message: chalk.yellow(" \n Please guess a number between 1-9: "),
     },
 ]);
 if (answer.userGuessingNumber === randomNumber) {
-    console.log("Congratulation! You guess the righ number");
-    console.log("you won the game");
+    console.log(chalk.blueBright("\n Congratulation! You guess the righ number"));
+    console.log(chalk.blueBright(" \n you won the game"));
+    console.log("_".repeat(50));
 }
 else {
-    console.log("you guess the wrong number");
-    console.log("Try again");
+    console.log(chalk.red("you guess the wrong number"));
+    console.log(chalk.blackBright("Try again"));
+    console.log("_".repeat(50));
 }
